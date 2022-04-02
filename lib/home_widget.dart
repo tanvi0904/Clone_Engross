e7e3b7c93bb6083ded8c00f40ce57212b7304d3f
FlutLab.io <>
2022-04-02 15:08:08 +0000

    Autosave

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'timer.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,11 +9,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State {
+  int _currentIndex = 0;
+  final List _children = [
+    TimerScreen(Colors.white),
+    TimerScreen(Colors.deepOrange),
+    TimerScreen(Colors.green)
+  ];
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0, // this will be set when a new tab is tapped
+        onTap: onTabTapped, // this will be set when a new tab is tapped
+        currentIndex: _currentIndex,
         showUnselectedLabels: true,
         selectedIconTheme: IconThemeData(color: Colors.white, size: 25, opacity: .8),
         unselectedIconTheme: IconThemeData(color: Colors.white, size: 25, opacity: .10),
